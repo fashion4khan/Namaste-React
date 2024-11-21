37850ac904ec2bba26622934404a25dc90d565af
fashion4khan <135121711+fashion4khan@users.noreply.github.com>
2024-11-21 17:10:11 +0000

    container config done

---
+++ lecture-1/App.js
@@ -0,0 +1,4 @@
+const df = React.createElement("h1", {id : "heading", abc: "xyz"}, "hello world form react !");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(df);
